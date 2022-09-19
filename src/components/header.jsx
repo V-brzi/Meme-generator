ba6d23c6fa56a667ee3b59cb42f9c8ca869e1be0
V-brzi <vatro.brzi@gmail.com>
2022-09-19 08:55:12 +0200

    update meme-gen

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,9 @@ function Header() {
         <header>
             <img src="https://th.bing.com/th/id/OIP.h0fNsIDqht_R54CjV5jpvwAAAA?pid=ImgDet&rs=1" alt="meme-icon" className="meme-icon"/>
             <h1 className="header-title">Meme Generator</h1>
-            <p className="header-txt">React Course - Project 3</p>
+            <p className="header-txt">
+                You <span className="meme-it">see</span> it you <span className="meme-it">meme</span> it
+            </p>
         </header>
     )
 }
